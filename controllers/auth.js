fecf937006520c211cca51e801aee1877c0eecb0
Brandon <brandoncasa8@gmail.com>
2023-01-23 04:06:47 -0500

    login

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,7 +6,7 @@ import { User } from "../models/User.js";
 export const register = async (req, res) => {
   try {
     const { displayname, username, email, password, interests } = req.body;
-    const salt = await bcrypt.genSalt(10);
+    const salt = await bcrypt.genSalt();
     const hashedPassword = await bcrypt.hash(password, salt);
     const newUser = new User({
       displayname,
@@ -21,3 +21,21 @@ export const register = async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 }
+
+// Logging in a user
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    !user && res.status(400).json({ msg: "User not found" });
+
+    const validated = await bcrypt.compare(password, user.password);
+    !validated && res.status(400).json({ msg: "Wrong password" });
+
+    const accessToken = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    delete user.password;
+    res.status(200).json({ accessToken, user });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
