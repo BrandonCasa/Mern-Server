89e83b56a8f8113ca2e38d9ee886a66d5035ce8d
Brandon <brandoncasa8@gmail.com>
2023-01-24 10:56:03 -0500

    friend operations

--- controllers/user.js
+++ controllers/user.js
@@ -54,10 +54,61 @@ export const setInterests = async (req, res) => {
   }
 }
 
+// Clean up a user's friend requests by removing any that are invalid
+export const cleanFriendRequests = async (_id) => {
+  try {
+    const user = await User.findById(_id);
+    let newFriendRequestsIn = [];
+    let newFriendRequestsOut = [];
+    for (let i = 0; i < user.friendRequestsIn.length; i++) {
+      if (mongoose.Types.ObjectId.isValid(user.friendRequestsIn[i])) {
+        const friend = await User.findById(user.friendRequestsIn[i]);
+        if (friend && friend.friendRequestsOut.includes(_id.toString())) {
+          newFriendRequestsIn.push(user.friendRequestsIn[i]);
+        }
+      }
+    }
+    for (let i = 0; i < user.friendRequestsOut.length; i++) {
+      if (mongoose.Types.ObjectId.isValid(user.friendRequestsOut[i])) {
+        const friend = await User.findById(user.friendRequestsOut[i]);
+        if (friend && friend.friendRequestsIn.includes(_id.toString())) {
+          newFriendRequestsOut.push(user.friendRequestsOut[i]);
+        }
+      }
+    }
+    const newUser = await User.findByIdAndUpdate(_id, { friendRequestsIn: newFriendRequestsIn, friendRequestsOut: newFriendRequestsOut }, { new: true });
+    return newUser;
+  } catch (err) {
+    throw new Error(err);;
+  }
+}
+
+// Clean up a user's friend list by removing any that are invalid or not mutual
+export const cleanFriends = async (_id) => {
+  try {
+    const user = await User.findById(_id);
+    let newFriends = [];
+    for (let i = 0; i < user.friends.length; i++) {
+      if (mongoose.Types.ObjectId.isValid(user.friends[i])) {
+        const friend = await User.findById(user.friends[i]);
+        if (friend && friend.friends.includes(_id.toString())) {
+          newFriends.push(user.friends[i]);
+        }
+      }
+    }
+    const newUser = await User.findByIdAndUpdate(_id, { friends: newFriends }, { new: true });
+    return newUser;
+  } catch (err) {
+    throw new Error(err);;
+  }
+}
+
 // Add a friend
 export const addFriend = async (req, res) => {
   try {
     const { _id } = req.user;
+    req.user = await cleanFriendRequests(_id);
+    req.user = await cleanFriends(_id);
     const { friendId } = req.params;
     if (!mongoose.Types.ObjectId.isValid(friendId)) {
       return res.status(404).json({ message: "User not found" });
@@ -79,6 +130,115 @@ export const addFriend = async (req, res) => {
     await User.findByIdAndUpdate(_id, { friendRequestsOut: [...user.friendRequestsOut, friendId] })
     await User.findByIdAndUpdate(friendId, { friendRequestsIn: [...friend.friendRequestsIn, _id.toString()] })
     return res.status(200).json({ message: "Friend request sent" });
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
+}
+
+// Accept a friend request
+export const acceptFriendRequest = async (req, res) => {
+  try {
+    const { _id } = req.user;
+    req.user = await cleanFriendRequests(_id);
+    req.user = await cleanFriends(_id);
+    const { friendId } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(friendId)) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const friend = await User.findById(friendId);
+    if (!friend) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const user = await User.findById(_id);
+    if (!user.friendRequestsIn.includes(friendId)) {
+      return res.status(400).json({ message: "You do not have a friend request from this user" });
+    }
+    await User.findByIdAndUpdate(_id, { friendRequestsIn: user.friendRequestsIn.filter(id => id !== friendId), friends: [...user.friends, friendId] })
+    await User.findByIdAndUpdate(friendId, { friendRequestsOut: friend.friendRequestsOut.filter(id => id !== _id.toString()), friends: [...friend.friends, _id.toString()] })
+    return res.status(200).json({ message: "Friend request accepted" });
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
+}
+
+// Decline a friend request
+export const declineFriendRequest = async (req, res) => {
+  try {
+    const { _id } = req.user;
+    req.user = await cleanFriendRequests(_id);
+    req.user = await cleanFriends(_id);
+    const { friendId } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(friendId)) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const friend = await User.findById(friendId);
+    if (!friend) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const user = await User.findById(_id);
+    if (!user.friendRequestsIn.includes(friendId)) {
+      return res.status(400).json({ message: "You do not have a friend request from this user" });
+    }
+    await User.findByIdAndUpdate(_id, { friendRequestsIn: user.friendRequestsIn.filter(id => id !== friendId) })
+    await User.findByIdAndUpdate(friendId, { friendRequestsOut: friend.friendRequestsOut.filter(id => id !== _id.toString()) })
+    return res.status(200).json({ message: "Friend request declined" });
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
+}
+
+// Cancel a friend request
+export const cancelFriendRequest = async (req, res) => {
+  try {
+    const { _id } = req.user;
+    req.user = await cleanFriendRequests(_id);
+    req.user = await cleanFriends(_id);
+    const { friendId } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(friendId)) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const friend = await User.findById(friendId);
+    if (!friend) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const user = await User.findById(_id);
+    if (!user.friendRequestsOut.includes(friendId)) {
+      return res.status(400).json({ message: "You do not have a friend request to this user" });
+    }
+    await User.findByIdAndUpdate(_id, { friendRequestsOut: user.friendRequestsOut.filter(id => id !== friendId) })
+    await User.findByIdAndUpdate(friendId, { friendRequestsIn: friend.friendRequestsIn.filter(id => id !== _id.toString()) })
+    return res.status(200).json({ message: "Friend request canceled" });
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
+}
+
+// Remove a friend
+export const removeFriend = async (req, res) => {
+  try {
+    const { _id } = req.user;
+    req.user = await cleanFriendRequests(_id);
+    req.user = await cleanFriends(_id);
+    const { friendId } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(friendId)) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const friend = await User.findById(friendId);
+    if (!friend) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const user = await User.findById(_id);
+    if (!user.friends.includes(friendId)) {
+      return res.status(400).json({ message: "You are not friends with this user" });
+    }
+    if (!friend.friends.includes(_id.toString())) {
+      // Remove friend from current user's friends list if friend is not friends with current user
+      await User.findByIdAndUpdate(_id, { friends: user.friends.filter(id => id !== friendId) })
+      return res.status(400).json({ message: "You are not friends with this user" });
+    }
+    await User.findByIdAndUpdate(_id, { friends: user.friends.filter(id => id !== friendId) })
+    await User.findByIdAndUpdate(friendId, { friends: friend.friends.filter(id => id !== _id.toString()) })
+    return res.status(200).json({ message: "Friend removed" });
   } catch (err) {
     return res.status(500).json({ error: err.message });
   }
--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addFriend, getUser, setAboutMe, setDisplayName, setInterests } from "../controllers/user.js";
+import { acceptFriendRequest, addFriend, cancelFriendRequest, declineFriendRequest, getUser, removeFriend, setAboutMe, setDisplayName, setInterests } from "../controllers/user.js";
 import { verifyToken } from "../middleware/auth.js";
 import { User } from "../models/User.js";
 import passport from "passport";
@@ -13,8 +13,11 @@ router.get("/profile/:userId", getUser);
 router.get("/me/changename", verifyToken, setDisplayName);
 router.get("/me/changeaboutme", verifyToken, setAboutMe);
 router.get("/me/changeinterests", verifyToken, setInterests);
-router.post("/friend/add/:friendId", verifyToken, addFriend);
-// router.patch("/friend/remove/:friendId", verifyToken, removeFriend);
+router.patch("/friend/add/:friendId", verifyToken, addFriend);
+router.patch("/friend/remove/:friendId", verifyToken, removeFriend);
+router.patch("/friend/accept/:friendId", verifyToken, acceptFriendRequest);
+router.patch("/friend/decline/:friendId", verifyToken, declineFriendRequest);
+router.patch("/friend/cancel/:friendId", verifyToken, cancelFriendRequest);
 
 
 export default router;
